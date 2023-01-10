43b5d2f09ca9a3f23419a555a21a9c6af2080af1
indlukula <84289216+indlukula@users.noreply.github.com>
2023-01-10 14:20:49 +0200

    Sifundo-adding routes arguments. Deleted procfile

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,11 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
   res.send('Sifundo Maphosa');
 });
+routes.get('/test', (req, res) => {
+  res.send('Jason Stantham');
+});
+routes.get('/test1', (req, res) => {
+  res.send('Will Smith');
+});
 
 module.exports = routes;
